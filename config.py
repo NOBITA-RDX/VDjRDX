6251bf89e16aaabbbb18aa68d4692a060f260986
RDX RAJ <141653116+NOBITA-RDX@users.noreply.github.com>
2024-01-27 16:22:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # Add Owner Username without @ 
-OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_RJ29RDX_ll"
+OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_RJ29RDX_ll")
 # Get Your bot username
 BOT_USERNAME = getenv("BOT_USERNAME", "RdxMusic1Bot")
 # Don't Add style font 
