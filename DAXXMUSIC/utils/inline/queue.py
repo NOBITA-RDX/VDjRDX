4840208a8f78e121a0783cbb950ad7d4331682c9
RDX RAJ <141653116+NOBITA-RDX@users.noreply.github.com>
2024-01-27 22:55:32 +0530

    Update queue.py

--- DAXXMUSIC/utils/inline/queue.py
+++ DAXXMUSIC/utils/inline/queue.py
@@ -73,7 +73,7 @@ def aq_markup(_, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [InlineKeyboardButton(text="๏ᴡɴєꝛ", user_id="1777270311"),
-        ],[InlineKeyboardButton(text="๏ᴡɴєꝛ", user_id="6232544403"),
+         InlineKeyboardButton(text="๏ᴡɴєꝛ", user_id="6232544403"),
         ],
     ]
     return buttons
